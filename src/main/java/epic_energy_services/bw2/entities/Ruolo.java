6981724c688fadd68daff4fe35fa229f7f8db38e
Sergio-dev01 <sergio.develoop@gmail.com>
2025-08-04 14:56:47 +0200

    add Ruolo and Utenti entities

---
+++ src/main/java/epic_energy_services/bw2/entities/Ruolo.java
@@ -0,0 +1,22 @@
+package epic_energy_services.bw2.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ruolo")
+@Getter
+public class Ruolo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_ruolo")
+    private String nomeRuolo;
+
+    @ManyToMany(mappedBy = "ruoli")
+    private Set<Utenti> utenti = new HashSet<>();
+}
