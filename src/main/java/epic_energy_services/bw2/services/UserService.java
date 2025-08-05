faeee025558f043c9030bd1e1b8e6e5d5561c2e7
iTino7 <Sabatino.b007@outlook.it>
2025-08-05 17:06:19 +0200

    fix

--- src/main/java/epic_energy_services/bw2/services/UserService.java
+++ src/main/java/epic_energy_services/bw2/services/UserService.java
@@ -2,11 +2,16 @@ package epic_energy_services.bw2.services;
 
 import epic_energy_services.bw2.entities.User;
 import epic_energy_services.bw2.exception.BadRequestException;
+import epic_energy_services.bw2.exception.NotFoundException;
 import epic_energy_services.bw2.payloads.NewUserDTO;
 import epic_energy_services.bw2.repositories.RuoloRepository;
 import epic_energy_services.bw2.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -30,46 +35,47 @@ public class UserService {
         return newUtente;
     }
 
-//    public User findById(Long userId) {
-//        return this.userRepository.findById(userId).orElseThrow(() -> new NotFoundException(userId));
-//    }
+    public User findById(Long userId) {
+        return this.userRepository.findById(userId).orElseThrow(() -> new NotFoundException("messaggio " + userId));
+    }
 
-//    public User findByIdAndUpdate(Long userId, NewUserDTO payload) {
-//
-//        User found = this.findById(userId);
-//
-//
-//        if (!found.getEmail().equals(payload.email()))
-//            this.userRepository.findByEmail(payload.email()).ifPresent(user -> {
-//                throw new BadRequestException("L'email " + user.getEmail() + " è già in uso!");
-//            });
-//
-//        found.setNome(payload.firstName());
-//        found.setCognome(payload.lastName());
-//        found.setEmail(payload.email());
-//        found.setPassword(payload.password());
-//        found.setAvatar("https://ui-avatars.com/api/?name=" + payload.firstName() + "+" + payload.lastName());
-//
-//        User modifiedUser = this.userRepository.save(found);
-//
-//        log.info("L'utente con id " + found.getId() + " è stato modificato!");
-//
-//        return modifiedUser;
-//    }
-//
-//    public void findByIdAndDelete(Long userId) {
-//        User found = this.findById(userId);
-//        this.userRepository.delete(found);
-//    }
-//    public Page<User> findAll(int pageNumber, int pageSize, String sortBy) {
-//        if (pageSize > 50) pageSize = 50;
-//        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy).descending());
-//        return this.userRepository.findAll(pageable);
-//    }
-//
-//    public User findByEmail(String email) {
-//        return this.userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("L'utente con l'email " + email + " non è stato trovato!"));
-//    }
+    public User findByIdAndUpdate(Long userId, NewUserDTO payload) {
+
+        User found = this.findById(userId);
+
+
+        if (!found.getEmail().equals(payload.email()))
+            this.userRepository.findByEmail(payload.email()).ifPresent(user -> {
+                throw new BadRequestException("L'email " + user.getEmail() + " è già in uso!");
+            });
+
+        found.setNome(payload.firstName());
+        found.setCognome(payload.lastName());
+        found.setEmail(payload.email());
+        found.setPassword(payload.password());
+        found.setAvatar("https://ui-avatars.com/api/?name=" + payload.firstName() + "+" + payload.lastName());
+
+        User modifiedUser = this.userRepository.save(found);
+
+        log.info("L'utente con id " + found.getId() + " è stato modificato!");
+
+        return modifiedUser;
+    }
+
+    public void findByIdAndDelete(Long userId) {
+        User found = this.findById(userId);
+        this.userRepository.delete(found);
+    }
+
+    public Page<User> findAll(int pageNumber, int pageSize, String sortBy) {
+        if (pageSize > 50) pageSize = 50;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy).descending());
+        return this.userRepository.findAll(pageable);
+    }
+
+    public User findByEmail(String email) {
+        return this.userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("L'utente con l'email " + email + " non è stato trovato!"));
+    }
 
 
 }
