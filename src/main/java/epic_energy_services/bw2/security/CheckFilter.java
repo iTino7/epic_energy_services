0a31f475d4c8c5ea9a9372a087b44b0c54142f7d
Sergio-dev01 <sergio.develoop@gmail.com>
2025-08-05 12:03:37 +0200

    add userDetails and Authorization

--- src/main/java/epic_energy_services/bw2/security/CheckFilter.java
+++ src/main/java/epic_energy_services/bw2/security/CheckFilter.java
@@ -1,12 +1,19 @@
 package epic_energy_services.bw2.security;
 
+import epic_energy_services.bw2.entities.User;
 import epic_energy_services.bw2.exception.UnauthorizedException;
+import epic_energy_services.bw2.services.UserService;
 import epic_energy_services.bw2.tools.JWTTools;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,6 +25,9 @@ public class CheckFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTTools jwtTools;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -33,6 +43,20 @@ public class CheckFilter extends OncePerRequestFilter {
         jwtTools.checkTokenValidity(accessToken);
 
         // ************ AUTORIZZAZIONE ************
+
+        String userId = jwtTools.getIdFromToken(accessToken);
+
+        User currentUser = this.userService.findById(Long.parseLong(userId));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                currentUser,
+                null,
+                currentUser.getAuthorities()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
     }
 
     @Override
