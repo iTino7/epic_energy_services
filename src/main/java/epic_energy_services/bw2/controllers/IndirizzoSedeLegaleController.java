768563cc317f5e21ba5e63c761a2e106dbcc8ce6
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-08-06 09:16:33 +0200

    aggiunta degli indirizzi controller

--- src/main/java/epic_energy_services/bw2/controllers/IndirizzoSedeLegaleController.java
+++ src/main/java/epic_energy_services/bw2/controllers/IndirizzoSedeLegaleController.java
@@ -1,4 +1,25 @@
 package epic_energy_services.bw2.controllers;
 
+import epic_energy_services.bw2.repositories.ComuneRepository;
+import epic_energy_services.bw2.services.IndirizzoSedeLegaleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sedi-legali")
 public class IndirizzoSedeLegaleController {
+    private final IndirizzoSedeLegaleService service;
+    private final ComuneRepository comuneRepository;
+
+    public IndirizzoSedeLegaleController(IndirizzoSedeLegaleService service, ComuneRepository comuneRepository) {
+        this.service = service;
+        this.comuneRepository = comuneRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
 }
--- src/main/java/epic_energy_services/bw2/controllers/IndirizzoSedeOperativaController.java
+++ src/main/java/epic_energy_services/bw2/controllers/IndirizzoSedeOperativaController.java
@@ -1,4 +1,36 @@
 package epic_energy_services.bw2.controllers;
 
+import epic_energy_services.bw2.entities.Comune;
+import epic_energy_services.bw2.entities.IndirizzoSedeOperativa;
+import epic_energy_services.bw2.payloads.NewIndirizzoDTO;
+import epic_energy_services.bw2.repositories.ComuneRepository;
+import epic_energy_services.bw2.services.IndirizzoSedeOperativaService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sedi-operative")
 public class IndirizzoSedeOperativaController {
+    private final IndirizzoSedeOperativaService service;
+    private final ComuneRepository comuneRepository;
+
+    public IndirizzoSedeOperativaController(IndirizzoSedeOperativaService service, ComuneRepository comuneRepository) {
+        this.service = service;
+        this.comuneRepository = comuneRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> creaNuovaSedeOperativa(@RequestBody @Valid NewIndirizzoDTO dto) {
+        Comune comune = comuneRepository.findById(dto.comuneId())
+                .orElseThrow(() -> new IllegalArgumentException("comune non trovato"));
+
+        IndirizzoSedeOperativa nuovo = service.creaNuovaSedeOperativa(dto, comune);
+        return ResponseEntity.ok(nuovo);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
 }
